228c0265d21e72d9dcbf16174882205e9318773b
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-06-21 11:40:35 +0545

    feat: implement StudentManager class and AddStudent method

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public record Student(int Id, string Name, string Grade);
+
+public class StudentManager
+{
+    private readonly List<Student> _students = new();
+    private int _nextId = 1;
+
+    public void AddStudent(string? name, string? grade)
+    {
+        string fullName = string.IsNullOrWhiteSpace(name) ? "Unknown" : name;
+
+        string grage = string.IsNullOrWhiteSpace(grade) ? "N/A" : grade;
+
+        _students.Add(new Student(_nextId++, fullName, grage));
+        Console.WriteLine("Student added successfully.");
 
 
+    }
 
 
+}
